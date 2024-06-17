fb3515aa0b77394b2c6c3972b846614e2e7bc46a
Uche <uuchei2@illinois.edu>
2024-06-17 09:30:56 -0500

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx'
 
-export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    images: {
+        unoptimized: true,
+    }
+};
+
+const withMDX = createMDX({
+    // Add markdown plugins here, as desired
+    options: {
+    },
+})
+   
+  // Wrap MDX and Next.js config with each other
+export default withMDX(nextConfig)
+
